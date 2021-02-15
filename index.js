afb934b0a322a9008738a32f52c8f8514cf36109
Dario Zubaray <dzubaray@gmail.com>
2021-02-15 00:06:19 -0300

    adding dotenv

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

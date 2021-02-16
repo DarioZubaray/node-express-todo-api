887c94b1b4f1c3ce0cfd228ad31df6b5300cf8a6
Dario Zubaray <dzubaray@gmail.com>
2021-02-16 12:00:34 -0300

    validating Todo's fields

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,11 +1,21 @@
 const { Router } = require('express')
-const { getTodos, crearTodo, getTodo, actualizarTodo, borrarTodo } = require('../controllers/todo')
+const { getTodos, crearTodo, getTodo, actualizarTodo, borrarTodo } = require('../controllers/todo');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router()
 
 router.get('/all', getTodos)
 router.get('/findBy/:id', getTodo)
-router.post('/', crearTodo)
+router.post(
+    '/',
+    [
+        check('message','El mensaje es obligatorio').not().isEmpty(),
+        check('finished','El estado terminado es obligatorio').not().isEmpty(),
+        check('datetime','La fecha es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearTodo)
 router.put('/:id', actualizarTodo)
 router.delete('/:id', borrarTodo)
 
